9448b819f2ba9f2d921a40b456eddccd760865eb
PavelZykan <pavel.zykan@gmail.com>
2018-03-18 21:08:34 +0100

    start/stop mode

--- timer-v01.ino
+++ timer-v01.ino
@@ -4,25 +4,39 @@
 #include <LiquidMenu.h>
 #include <buttons.h>
 
+#define swVersion "1.0"
 #define buzzerPin 3
 #define settingsAddress 0
+#define buzzerToneLength 500
+#define elapsedTimeMenuUpdateDelay 200
 
 struct BuzzerSettings {
   unsigned short timerDelayMin;
   unsigned short timerDelayMax;
 };
 
-unsigned long startPressed = 0L;
+bool countdownRunning = false;
+unsigned long countdownStart = 0L;
 unsigned int buzzerDelay;
+bool buzzerRunning = false;
+bool startStopModeRunning = false;
+unsigned long buzzerStarted = 0L;
+float elapsedTime = 0.0f;
+unsigned long lastElapsedTimeMenuUpdate = 0L;
+char* secondsConstant;
 
 BuzzerSettings currentSettings;
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);  // Set the LCD I2C address
 
-LiquidLine welcome_line1(1, 0, "LiquidMenu ", LIQUIDMENU_VERSION);
-LiquidLine welcome_line2(1, 1, "Hello Menu I2C");
+LiquidLine welcome_line1(0, 0, "ArTimer ", swVersion);
+LiquidLine welcome_line2(0, 1, "Run: UP or DOWN");
 
 LiquidScreen welcome_screen(welcome_line1, welcome_line2);
+
+LiquidLine startStopLine1(0, 0, "Start/Stop mode");
+LiquidLine startStopLine2(0, 1, "Time: ", elapsedTime, secondsConstant);
+LiquidScreen startStopScreen(startStopLine1, startStopLine2);
 
 LiquidLine timerDelayMinLine1(0, 0, "Timer delay");
 LiquidLine timerDelayMinLine2(0, 1, "Min: ", currentSettings.timerDelayMin);
